b7166e40a1df91c4f06f7088a4efbd307ca3bb5a
Xong Xiong <xxiong3153@gmail.com>
2017-10-13 17:06:45 -0500

    added click handler and formatted DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,32 @@
 $(document).ready(main);
 
 function main() {
-    console.log('jquery/JS loaded');
+    clickHandler();    
 }
+
+function clickHandler() {
+    $('#operators').on('click','#add', adding);
+    $('#operators').on('click','#subtract', subtracting);
+    $('#operators').on('click','#multiply', multiplying);
+    $('#operators').on('click','#divide', dividing);
+}
+
+var operator = '';
+
+function adding(){
+    operator = '+';
+}
+
+function subtracting(){
+    operator = '-';
+}
+
+function multiplying(){
+    operator = '*';
+  
+}
+
+function dividing(){
+    operator = '/';
+}
+
