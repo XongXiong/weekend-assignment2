750ff895bd2a868bc6d535946a298a3a6589cf20
Xong Xiong <xxiong3153@gmail.com>
2017-10-14 13:56:13 -0500

    calculator functioning

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,14 +1,17 @@
 $(document).ready(main);
 
 function main() {
-    clickHandler();    
+    clickHandler();
 }
 
 function clickHandler() {
-    $('#operators').on('click','#add', adding);
-    $('#operators').on('click','#subtract', subtracting);
-    $('#operators').on('click','#multiply', multiplying);
-    $('#operators').on('click','#divide', dividing);
+    // For Operators
+    $('#add').on('click', adding);
+    $('#subtract').on('click', subtracting);
+    $('#multiply').on('click', multiplying);
+    $('#divide').on('click', dividing);
+    // For posting to server 
+    $('#calculate').on('click', calculate)
 }
 
 var operator = '';
@@ -30,3 +33,23 @@ function dividing(){
     operator = '/';
 }
 
+function calculate() {
+    var x = $('#x').val();
+    var y = $('#y').val();
+    $.ajax({
+        method: 'POST',
+        url: '/calculator',
+        data: {x, y, operator}
+    }) .done(function(response){
+        console.log('calculate POST:', response);
+        result(response);
+    }) .fail(function(message){
+        console.log('error', message);
+    })
+}
+
+function result(response){
+    var result = response.result;
+    console.log(result);
+    $('#result').text("Result = " + result);
+}
--- server/server.js
+++ server/server.js
@@ -7,6 +7,32 @@ app.use(express.static('server/public'));
 
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.post('/calculator', function(req, res) {
+    console.log(req.body);
+    var x = parseInt(req.body.x);
+    var y = parseInt(req.body.y);
+    var operator = req.body.operator;
+    var result = 0;
+    switch(operator) {
+        case '+':
+            result = x + y;
+            break;
+        case '-':
+            result = x - y;
+            break;
+        case '*':
+            result = x * y;
+            break;
+        case '/':
+            result = x / y;
+            break;
+    }
+    console.log(result);
+    resultObj = {result}
+    console.log(resultObj);
+    res.send(resultObj);
+}) 
+
 app.listen(port , function(){
     console.log('listening on port', port);
 })
